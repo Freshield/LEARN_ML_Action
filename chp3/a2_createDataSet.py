6b59026b1754455fceee4d58a8e75cba35d251f8
Freshield <zxdsw199182@gmail.com>
2019-11-23 11:28:27 +0800

    interrupt

--- chp3/a2_createDataSet.py
+++ chp3/a2_createDataSet.py
@@ -2,9 +2,9 @@
 """
 @Author: Freshield
 @Contact: yangyufresh@163.com
-@File: a1_calcShannonEnt.py
-@Time: 2020-03-12 11:23
-@Last_update: 2020-03-12 11:23
+@File: a2_createDataSet.py
+@Time: 2020-03-12 11:27
+@Last_update: 2020-03-12 11:27
 @Desc: None
 @==============================================@
 @      _____             _   _     _   _       @
@@ -14,8 +14,16 @@
 @                                    Freshield @
 @==============================================@
 """
-from math import log
 
 
-def calcShannonEnt(dataSet):
-    numEntries = len(dataSet)
+def createDataSet():
+    dataSet = [
+        [1,1,'yes'],
+        [1,1,'yes'],
+        [1,0,'no'],
+        [0,1,'no'],
+        [0,1,'no']
+    ]
+    lables = ['no surfacing', 'flippers']
+
+    return dataSet, lables
