8c890aa618dc48a5ccdd2437273027769b9a2a3b
Freshield <zxdsw199182@gmail.com>
2019-11-26 14:00:03 +0800

    done calcShannonEnt

--- chp3/a1_calcShannonEnt.py
+++ chp3/a1_calcShannonEnt.py
@@ -18,4 +18,28 @@ from math import log
 
 
 def calcShannonEnt(dataSet):
+    # 一共有多少数据
     numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+
+    return shannonEnt
+
+
+if __name__ == '__main__':
+    from a2_createDataSet import createDataSet
+    dataSet, labels = createDataSet()
+    # dataSet[0][-1] = 'maybe'
+    print(dataSet)
+    print(labels)
+    shannonEnt = calcShannonEnt(dataSet)
+    print(shannonEnt)
