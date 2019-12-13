1c8fed9fc6980ab85bdfddd2d71c6a4533e60090
Freshield <zxdsw199182@gmail.com>
2019-12-13 17:35:18 +0800

    done the bag of words to vec

---
+++ chp4/bagOfWords2Vec.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: bagOfWords2Vec.py
+@Time: 2019-12-12 17:33
+@Last_update: 2019-12-12 17:33
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+
+
+def bagOfWords2Vec(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+
+    return returnVec
