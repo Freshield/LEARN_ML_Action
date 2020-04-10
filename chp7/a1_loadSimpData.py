dac816fb53e129802c048f5243193291d3f3a8d4
Freshield <zxdsw199182@gmail.com>
2020-04-10 14:21:43 +0800

    begin today' git

---
+++ chp7/a1_loadSimpData.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a1_loadSimpData.py
+@Time: 2020-04-10 14:17
+@Last_update: 2020-04-10 14:17
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+from numpy import *
+
+
+def loadSimpData():
+    datMat = matrix([[1., 2.1],
+                     [2., 1.1],
+                     [1.3, 1.],
+                     [1., 1.],
+                     [2., 1.]])
+    classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+
+
+    return datMat, classLabels
