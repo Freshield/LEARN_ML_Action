7556ce7f7375563cd4fb68d4fd322a3ce8c9228c
Freshield <zxdsw199182@gmail.com>
2020-06-24 14:27:08 +0800

    done the pca try part

--- chp13/a1_pca.py
+++ chp13/a1_pca.py
@@ -14,3 +14,38 @@
 @                                    Freshield @
 @==============================================@
 """
+from numpy import *
+
+
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    datArr = [list(map(float, line)) for line in stringArr]
+
+    return mat(datArr)
+
+
+def pca(dataMat, topNfeat=9999999):
+    meanVals = mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    convMat = cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = linalg.eig(mat(convMat))
+    eigValInd = argsort(eigVals)
+    eigValInd = eigValInd[:-(topNfeat+1):-1]
+    redEigVects = eigVects[:, eigValInd]
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+
+    return lowDDataMat, reconMat
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    dataMat = loadDataSet('data/testSet.txt')
+    lowDmat, reconMat = pca(dataMat, 1)
+    print(shape(lowDmat))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker='^', s=90)
+    ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s=50, c='red')
+    plt.show()
